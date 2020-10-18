c5e1b5bd2f6b4c574f77bbf381b0c41fd79af285
unknown <boureimatapily1@gmail.com>
2020-10-18 18:12:25 +0000

    MOD change directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { FooterComponent } from './footer/footer.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -8,7 +9,6 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { ContactComponent } from './contact/contact.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { AboutComponent } from './about/about.component';
-import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
