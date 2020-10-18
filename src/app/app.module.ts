5c75cb38fea1817c4364740c87e94a5068d58c45
unknown <boureimatapily1@gmail.com>
2020-10-18 20:42:25 +0000

    FIX Bug and ADD Image

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,23 @@
+import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ContactComponent } from './contact/contact.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { AboutComponent } from './about/about.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     NavbarComponent,
     ContactComponent,
     NotFoundComponent,
-    AboutComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
